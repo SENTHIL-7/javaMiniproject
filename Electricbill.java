15054a9b3046c97ff3d9eb03af9375fa2ac7687e
SENTHIL-7 <82882024+SENTHIL-7@users.noreply.github.com>
2021-08-18 13:11:34 +0530

    Update Electricbill.java

--- Electricbill.java
+++ Electricbill.java
@@ -22,7 +22,7 @@ package com.eBill;
 import java.io.*;
 import java.util.*;
 class Electricbill
-{                                                                 // throws IOException
+{                                                                 
 public static void main(String args[]) throws IOException
 {
 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
